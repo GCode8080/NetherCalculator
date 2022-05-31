bcbcf97225e5955313b9176424922ae40dadbd1f
Kibblezmeow <86687524+Kibblezmeow@users.noreply.github.com>
2022-05-30 19:03:32 -0800

    added stuff

---
+++ nether_calculator.py
@@ -0,0 +1,14 @@
+import time
+
+print("What is your X cordinate?")
+
+x = int(input("X cordinate: "))
+
+print("What is your Z cordinate?")
+
+z = int(input("Z cordinate: "))
+
+print("your nether cordinates are: " + str(x/8) + " / " + str(z/8))
+
+
+time.sleep(20)
